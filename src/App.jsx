8057885f32019d4359cf35052e263a523eb3800e
Gourav <junejagourav2006@gmail.com>
2026-03-15 23:00:22 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Navbar from "./sections/Navbar";
 function App() {
-  return <div className="text-red-700">hello world</div>;
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
